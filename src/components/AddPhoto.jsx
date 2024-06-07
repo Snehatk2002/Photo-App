0df0e8d7f2c33dc961249c5e34210d322ea4bad5
Snehatk2002 <sammu5285@gmail.com>
2024-06-07 23:03:27 +0530

    completed

---
+++ src/components/AddPhoto.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const AddPhoto = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                           <label htmlFor="" className="form label">PHOTO ID</label> 
+                           <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                           <label htmlFor="" className="form label">PHOTO NAME</label> 
+                           <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                           <label htmlFor="" className="form label"> DESCRIPTION</label> 
+                         <textarea name="" id="" cols="30" rows="10" className="form-control"></textarea>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                           <label htmlFor="" className="form label">EMAIL ID</label> 
+                           <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                           <label htmlFor="" className="form label">PHONE NO</label> 
+                           <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                           <label htmlFor="" className="form label">PHOTO DATE</label> 
+                           <input type="date" name="" id="" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                           <label htmlFor="" className="form label">PHOTO SIZE</label> 
+                           <input type="text" className="form-control" />
+                        </div>
+                        <center>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                           <button class="btn btn-success">SUBMIT</button>
+                        </div>
+                        </center>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddPhoto
